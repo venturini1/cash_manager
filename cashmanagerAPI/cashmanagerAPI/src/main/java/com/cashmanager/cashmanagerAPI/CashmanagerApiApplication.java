8efac046b8bfd16af5ecc8a7684303cdcaf97115
Stenalo <stephane-desir.barro@epitech.eu>
2023-11-20 16:36:26 +0100

    commit backend spring security and boot
    Add spring setup security

---
+++ cashmanagerAPI/cashmanagerAPI/src/main/java/com/cashmanager/cashmanagerAPI/CashmanagerApiApplication.java
@@ -0,0 +1,14 @@
+package com.cashmanager.cashmanagerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashmanagerApiApplication {
+
+	public static void main(String[] args) {
+		System.out.print("Hello world");
+		SpringApplication.run(CashmanagerApiApplication.class, args);
+	}
+
+}
---
+++ cashmanagerAPI/cashmanagerAPI/src/test/java/com/cashmanager/cashmanagerAPI/CashmanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashmanager.cashmanagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashmanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
