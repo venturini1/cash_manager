4e2c54de3fd3a88e69c9163edae53ee7f541caad
Stenalo <60882835+Stenalo@users.noreply.github.com>
2023-12-18 16:02:54 +0100

    delete extra spaces

--- Cash_manager/app/build.gradle.kts
+++ Cash_manager/app/build.gradle.kts
@@ -90,9 +90,7 @@ dependencies {
     implementation ("androidx.navigation:navigation-compose:2.5.3")
 
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0-beta01")
-
     implementation ("androidx.core:core-ktx:1.9.0")
-
     implementation ("androidx.navigation:navigation-compose:2.7.5")
     implementation ("androidx.compose.material:material-icons-core")
     implementation ("com.google.mlkit:barcode-scanning:17.2.0")
