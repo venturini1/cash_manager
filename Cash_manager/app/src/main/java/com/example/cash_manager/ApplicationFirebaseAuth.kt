aab9b49c46a2bf517501749bc85fcc411cedf137
Stenalo <stephane-desir.barro@epitech.eu>
2023-12-19 15:32:51 +0100

    Add appauth to  manifest

--- Cash_manager/app/src/main/java/com/example/cash_manager/ApplicationFirebaseAuth.kt
+++ Cash_manager/app/src/main/java/com/example/cash_manager/ApplicationFirebaseAuth.kt
@@ -1,4 +1,9 @@
 package com.example.cash_manager
 
-class ApplicationFirebaseAuth {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ApplicationFirebaseAuth: Application() {
+
 }
---
+++ Cash_manager/app/src/main/java/com/example/cash_manager/DepInjection/Appmodule.kt
@@ -0,0 +1,9 @@
+package com.example.cash_manager.DepInjection
+
+import dagger.Module
+import dagger.hilt.InstallIn
+
+@Module
+@InstallIn()
+object Appmodule {
+}
