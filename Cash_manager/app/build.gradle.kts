a87a6e951374057c9f71ac502fbb56877492da31
Stenalo <stephane-desir.barro@epitech.eu>
2024-01-08 17:13:00 +0100

    Update build.gradle.kts

--- Cash_manager/app/build.gradle.kts
+++ Cash_manager/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 34
         targetSdk = 34
         versionCode = 1
-        versionName = "1.3"
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
